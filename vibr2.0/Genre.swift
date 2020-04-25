5f026ad263b28495cce62d04503238d034a55c3d
Pranay0302 <pka.pranayandra02@gmail.com>
2020-04-25 16:14:13 +0530

    Basic

---
+++ vibr2.0/Genre.swift
@@ -0,0 +1,42 @@
+//
+//  Genre.swift
+//  vibr2.0
+//
+//  Created by pranay andra on 25/04/20.
+//  Copyright © 2020 pranay andra. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class Genre: UIViewController {
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func Genretapped(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                self.playgenre(genre: sender.currentTitle!)
+            }
+        }
+    }
+    @IBAction func Stoptapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    @IBAction func Nexttapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()    
+    }
+    
+    func playgenre(genre: String) {
+        musicPlayer.stop()
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+}
